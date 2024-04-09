3f9fa596bbdbcb64f08aaef73c9c0460d95b0883
Amit Jha <91418427+jhaami@users.noreply.github.com>
2024-04-09 10:43:17 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,17 @@
-const http=require('http');
-const express= require('express');
-const cors=require('cors');
-const socketIO=require('socket.io');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const app=express();
-const port=4500 || process.env.PORT; //process.env.PORT means ... 4500 mila acha nahi to auto koi port utha lega
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-const users=[{}];
-app.use(cors());
- app.get("/",(req,res)=>{
-    res.send("HELL ITS WORKING");
- })
-
-// create a server
-const server=http.createServer(app);
-// io is connection 
-const io= socketIO(server);
-
-io.on("connection",(socket)=>{
-    console.log("New connection");
-
-    socket.on('joined',({user})=>{
-        users[socket.id]=user;
-        console.log("joined");
-        socket.broadcast.emit('userJoined',{user:"Admin",message:`${users[socket.id]} has joined`});
-        socket.emit('welcome',{user:"Admin", message:`Welcome to the chat, ${users[socket.id]} `});
-    })
-
-    socket.on('message',({message,id})=>{
-       io.emit('sendMessage',{user:users[id],message,id})
-    })
-  socket.on('disconnect',()=>{
-    socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]} has left`})
-    console.log(`user left`)
-  })
-    
-    
-});
-
-server.listen(port,()=>{
-    console.log(`server is working on http://localhost:${port}`);
-})
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
